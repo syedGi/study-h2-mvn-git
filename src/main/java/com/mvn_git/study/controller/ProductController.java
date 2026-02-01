28c65ce722a43e4260f012ce0ab15a06385ebe7e
ameen-se <ameeen.se24@gmail.com>
2026-02-01 12:36:33 +0530

    added MySQL

--- src/main/java/com/mvn_git/study/controller/ProductController.java
+++ src/main/java/com/mvn_git/study/controller/ProductController.java
@@ -7,12 +7,15 @@ import com.mvn_git.study.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RestController("/productdetails")
+//@RestController("/productdetails")
+@RestController
 @Slf4j
 public class ProductController {
 	
@@ -22,11 +25,11 @@ public class ProductController {
 		this.productService = productService;
 	}
 
-	@GetMapping()
-	public ResponseEntity<ProductDetails> getAllProducts() {
-		productService.getFullProducts();
-		log.info("=>GET ALL PRODUCT DETAILS DONE<=");
-		return ResponseEntity.ok().body(null);
+	@GetMapping("/get")
+	public ResponseEntity<List<ProductDetails>> getAllProducts() {
+		List<ProductDetails>pdList = productService.getFullProducts();
+		log.info("=>GET ALL PRODUCT DETAILS DONE<=::", pdList);
+		return ResponseEntity.ok().body(pdList);
 	}
 	
 }
