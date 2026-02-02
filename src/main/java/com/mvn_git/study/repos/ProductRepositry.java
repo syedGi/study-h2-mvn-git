a5a0c5b76911be96f32938defd96f8ef382dd9ec
ameen-se <ameeen.se24@gmail.com>
2026-02-02 10:18:09 +0530

    prod-repo

--- src/main/java/com/mvn_git/study/repos/ProductRepositry.java
+++ src/main/java/com/mvn_git/study/repos/ProductRepositry.java
@@ -1,6 +1,9 @@
 package com.mvn_git.study.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mvn_git.study.model.ProductDetails;
@@ -8,4 +11,7 @@ import com.mvn_git.study.model.ProductDetails;
 @Repository
 public interface ProductRepositry extends JpaRepository<ProductDetails, Long>{
 
+	@Query("SELECT * FROM PRODUCTDETAILS P WHERE "
+			+ "P.QUANTITY > (:NO) AND CATEGORY IN (:LIST)")
+	List<ProductDetails> findMyProds();
 }
